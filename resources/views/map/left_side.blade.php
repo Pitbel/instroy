62b4b039572e2f36eff39759f0e26309aeaa567e
Drunk Steep <dpalaydovich@gmail.com>
2019-03-04 13:12:05 +0300

    upd

--- resources/views/map/left_side.blade.php
+++ resources/views/map/left_side.blade.php
@@ -1,7 +1,9 @@
+<h4 class="text-center">Новости</h4>
+<hr/>
 @foreach($leftSide as $news)
     <div class="media align-items-center mb-2">
         <a href="/news/{{ $news->id }}/view">
-            <img width="64" class="img-thumbnail mr-3" src="{{ $news->img }}" alt="{{ $news->title }}">
+            <img width="64" class="rounded mr-3" src="{{ $news->img }}" alt="{{ $news->title }}">
         </a>
         <div class="media-body">
             <a href="/news/{{ $news->id }}/view"><h5 class="mt-0" style="font-size: 1em;">{{ $news->title }}</h5></a>
--- resources/views/map/right_side.blade.php
+++ resources/views/map/right_side.blade.php
@@ -1,7 +1,9 @@
+<h4 class="text-center">Участки</h4>
+<hr/>
 @foreach($rightSide as $land)
     <div class="media mt-2 align-items-center">
         <a href="/catalog/item/{{ $land->id }}/view">
-            <img width="64" class="img-thumbnail mr-3" src="{{ (isset($land->images[0])) ? $land->images[0]->img_link : '' }}" alt="{{ $land->name }}">
+            <img width="64" class="rounded mr-3" src="{{ (isset($land->images[0])) ? $land->images[0]->img_link : '' }}" alt="{{ $land->name }}">
         </a>
         <div class="media-body">
             <a href="/catalog/item/{{ $land->id }}/view"><h5 style="font-size: 1em;" class="mt-0">{{ $land->name }}</h5></a>
